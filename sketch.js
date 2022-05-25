fa06aa365c6aa6e27224588e2af2cf6b42596392
vbrowder18 <105737938+vbrowder18@users.noreply.github.com>
2022-05-25 10:25:06 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//var a1 = loadSound("A1.mp3")
+function setup() {
+  createCanvas(700, 300);
+   //a1.play();
+}
+
+function draw() {
+  background(255,255,255);
+  rect(150,70,435,150);
+  stroke(0,0,0);
+  rect (150,70, 80,150);
+  rect (220,70, 80,150);
+  rect (290,70, 80,150);
+  rect (360,70, 80,150);
+  rect (430,70, 80,150);
+  
+  rect (205,70,30,80);
+  rect (275,70,30,80);
+  rect (345,70, 30,80);
+  rect (415,70, 30,80);
+  rect (495,70, 30,80);
+  
+}
